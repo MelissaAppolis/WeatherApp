6b7762e429c1b03ec72df9659231953bd1792e5b
Melissa <melissa@Ambers-MacBook-Air.local>
2020-11-05 23:21:21 +0200

    changed API to https

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ class App extends Component {
     const city = e.target.elements.city.value;
     const country = e.target.elements.country.value;
     // Fetching api url from OpenWeather to get the current weather data
-    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`);
+    const api_call = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`);
     // Returning the data from url to json format
     const data = await api_call.json();
     // If city and country has been typed into the form from the user, this state with change
